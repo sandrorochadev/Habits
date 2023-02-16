ff43ed9885fdaf3d92322a75716cfd34ad099792
Alessandro Rocha <sandrorochadev>
2023-02-16 13:00:37 -0300

    feat: Configuração do banco de dados com Prisma

---
+++ server/src/server.ts
@@ -0,0 +1,18 @@
+import Fastify from 'fastify'
+import { PrismaClient } from '@prisma/client'
+import cors from '@fastify/cors'
+
+const app = Fastify()
+const prisma = new PrismaClient()
+app.register(cors)
+
+app.get('/hello', async () => {
+    const habits = await prisma.habits.findMany()
+    return habits
+})
+
+app.listen({
+    port: 3333,
+}).then(() => {
+    console.log('Server running on http://localhost:3333')
+})
